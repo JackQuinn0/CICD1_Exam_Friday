0b875e9c3963d6d32de7401f47bc2f988e4099f1
JackQuinn0 <G00395858@atu.ie>
2025-11-21 09:10:59 +0000

    Created new project

---
+++ src/main/java/ie/atu/Cicd1ExamFridayApplication.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1ExamFridayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1ExamFridayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/Cicd1ExamFridayApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1ExamFridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
